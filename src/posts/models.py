c0d91862bd968be47d5084c2cadfeedb6b994bbc
root <root@DESKTOP-L5I44QV.localdomain>
2020-12-13 02:53:12 +1100

    Git post search, posts and categories done

--- src/posts/models.py
+++ src/posts/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth import get_user_model
+from django.urls import reverse
 
 User = get_user_model()
 
@@ -20,7 +21,9 @@ class Post(models.Model):
     title = models.CharField(max_length=100)
     overview = models.TextField()
     timestamp = models.DateTimeField(auto_now_add = True)
+    location = models.TextField(default = "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d424146.1026392451!2d150.65179666970943!3d-33.847356724710885!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6b129838f39a743f%3A0x3017d681632a850!2sSydney%20NSW!5e0!3m2!1sen!2sau!4v1607782680258!5m2!1sen!2sau") # Map of Sydney
     comment_count = models.IntegerField(default = 0)
+    view_count = models.IntegerField(default = 0)
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
     thumbnail = models.ImageField()
     categories = models.ManyToManyField(Category)
@@ -28,3 +31,8 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+    def post_url(self):
+        return reverse('post_detail', kwargs={
+            'id': self.id
+        })
--- src/posts/views.py
+++ src/posts/views.py
@@ -1,5 +1,28 @@
+from django.db.models import Count, Q
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import render
 from .models import Post
+
+def search(request):
+    queryset = Post.objects.all()
+    query = request.GET.get('q')
+    
+    if query:
+        queryset = queryset.filter(
+            Q(title__icontains=query) |
+            Q(overview__icontains=query)
+        ).distinct()
+
+        context = {
+            'request': queryset
+        }
+        
+    return render(request, 'search_results.html', context)
+
+def get_cat_count():
+    queryset = Post.objects.values('categories__title').annotate(Count('categories__title'))
+
+    return queryset
 
 def index(request):
     featured_posts = Post.objects.filter(featured=True)
@@ -13,7 +36,29 @@ def index(request):
     return render(request, 'index.html', context)
 
 def blog(request):
-    return render(request, 'blog.html', {})
+    post_list = Post.objects.all()
+    latest_posts = Post.objects.order_by('-timestamp')[0:3]
+    cat_count = get_cat_count()
+    paginator = Paginator(post_list, 4)
+    page_request_var = 'page'
+    page = request.GET.get(page_request_var)
 
-def post(request):
+    try:
+        paginated_queryset = paginator.page(page)
+    except PageNotAnInteger:
+        paginated_queryset = paginator.page(1)
+    except EmptyPage:
+        paginated_queryset = paginator.page(paginator.num_pages)
+
+
+    context = {
+        'queryset': paginated_queryset,
+        'page_request_var': page_request_var,
+        'latest_posts': latest_posts,
+        'cat_count': cat_count
+    }
+
+    return render(request, 'blog.html', context)
+
+def post(request, id):
     return render(request, 'post.html', {})
