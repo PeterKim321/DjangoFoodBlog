ed37b99be0276a0ceb46d94721cf29bcc21cb932
Ubuntu <ubuntu@ip-172-31-41-76.ap-southeast-2.compute.internal>
2020-12-18 13:49:29 +0000

    secret key removed remote

--- src/blog/settings.py
+++ src/blog/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 
+from decouple import config
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -19,10 +21,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "w8j!i5&!&2&l5%sn+3h435vu22lr(e@qgtw8t#!1042yz=dl*8"
-
+SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = config('DEBUG')
 
 ALLOWED_HOSTS = ['*']
 
