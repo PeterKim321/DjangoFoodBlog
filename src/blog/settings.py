2420643997a95cf54503b33d43c8c0dca02896db
Ubuntu <ubuntu@ip-172-31-41-76.ap-southeast-2.compute.internal>
2020-12-18 12:24:22 +0000

    secret environment key adjusted for production, configured Apache serve media when debug = False

--- src/blog/settings.py
+++ src/blog/settings.py
@@ -15,15 +15,14 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ''
+SECRET_KEY = "w8j!i5&!&2&l5%sn+3h435vu22lr(e@qgtw8t#!1042yz=dl*8"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
